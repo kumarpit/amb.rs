c262cc4720c7ecb4a6c65f72e0aa54df9e1e44c5
krarpit <kumar.arpit77@yahoo.com>
2025-07-30 00:12:10 -0700

    map coloring exmaple

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,10 @@
 mod tests {
     use amb::amb;
 
+    ///////////////////////////
+    // Simple Examples
+    //////////////////////////
+
     #[test]
     fn it_works() {
         assert_eq!(
@@ -43,5 +47,71 @@ mod tests {
             .collect::<Vec<_>>(),
             vec![(5, 5, 5)]
         );
+    }
+
+    ///////////////////////////
+    // Map Coloring Example
+    //////////////////////////
+
+    use std::collections::{HashMap, HashSet};
+
+    #[test]
+    fn map_coloring() {
+        #[derive(Eq, PartialEq, Hash, Copy, Clone)]
+        enum Color {
+            Red,
+            Green,
+            Yellow,
+            Blue,
+        }
+
+        #[derive(Eq, PartialEq, Hash)]
+        enum Node {
+            A,
+            B,
+            C,
+            D,
+            E,
+            F,
+        }
+
+        let colors: HashSet<Color> =
+            HashSet::from([Color::Red, Color::Green, Color::Yellow, Color::Blue]);
+
+        let adjacency_list = HashMap::from([
+            (Node::A, vec![Node::B, Node::C, Node::D, Node::F]),
+            (Node::B, vec![Node::A, Node::C, Node::D]),
+            (Node::C, vec![Node::A, Node::B, Node::D, Node::E]),
+            (Node::D, vec![Node::A, Node::B, Node::C, Node::E, Node::F]),
+            (Node::E, vec![Node::C, Node::D, Node::F]),
+            (Node::F, vec![Node::A, Node::D, Node::E]),
+        ]);
+
+        amb!({
+            let a = choice!(&colors);
+            let b = choice!(&colors);
+            let c = choice!(&colors);
+            let d = choice!(&colors);
+            let e = choice!(&colors);
+            let f = choice!(&colors);
+
+            let assignment = HashMap::from([
+                (Node::A, &a),
+                (Node::B, &b),
+                (Node::C, &c),
+                (Node::D, &d),
+                (Node::E, &e),
+                (Node::F, &f),
+            ]);
+
+            for (node, neighbours) in adjacency_list {
+                for neighbour in neighbours {
+                    require!(assignment.get(&node).unwrap() != assignment.get(&neighbour).unwrap());
+                }
+            }
+
+            return (*a, *b, *c, *d, *e, *f);
+        })
+        .next();
     }
 }
